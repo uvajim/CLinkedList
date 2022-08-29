d9ce1efb3b7d2d149be741a91ffa361d0df99cf1
Jim Liang <89550877+uvajim@users.noreply.github.com>
2022-08-29 15:18:24 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+
+	file = fopen(argv[2], 'r');
+	
+    
+	if (argc != 3){
+	 	printf("Only 2 arguements should be applied");
+	}
+	else (argv[1] == 'echo'){
+		
+	}
+}
